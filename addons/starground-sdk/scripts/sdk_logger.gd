2630d17d693c5e0f49f4b788fe5739b64ffb5232
mblastergamer <mblastergamerelcapo@gmail.com>
2024-10-12 18:56:44 -0300

    Added SDK Output Panel and internal message logic
    Added the SDK Output panel to the bottom tabs of the editor, and made it functional with a singleton that handles message logic.

---
+++ addons/starground-sdk/scripts/sdk_logger.gd
@@ -0,0 +1,96 @@
+@tool
+extends Node
+
+
+signal info_count_changed(new_count: int)
+signal warning_count_changed(new_count: int)
+signal error_count_changed(new_count: int)
+
+enum MessageType {
+	INFO,
+	WARNING,
+	ERROR
+}
+
+var current_info_count: int = 0
+var current_warning_count: int = 0
+var current_error_count: int = 0
+var Output: RichTextLabel
+var messages: Array[Message]
+
+
+
+
+
+func _exit_tree() -> void:
+	for message in messages:
+		message.free()
+
+
+func info(message: String) -> void:
+	_add_message(message, MessageType.INFO)
+
+
+func warn(message: String) -> void:
+	_add_message(message, MessageType.WARNING)
+
+
+func error(message: String) -> void:
+	_add_message(message, MessageType.ERROR)
+
+
+func filter(filters: Array[MessageType], search: String) -> void:
+	Output.clear()
+	for message in messages:
+		if message.type in filters and (search == "" or search.to_lower() in message.text.to_lower()):
+			_show_message(message)
+
+
+func clear() -> void:
+	Output.clear()
+	for message in messages:
+		message.free()
+	messages.clear()
+
+
+func _add_message(text: String, type: MessageType) -> void:
+	match type:
+		MessageType.INFO:
+			current_info_count += 1
+			info_count_changed.emit(current_info_count)
+		MessageType.WARNING:
+			current_warning_count += 1
+			warning_count_changed.emit(current_warning_count)
+		MessageType.ERROR:
+			current_error_count += 1
+			error_count_changed.emit(current_error_count)
+	
+	var new_message = Message.new(text, type)
+	messages.append(new_message)
+	_show_message(new_message)
+
+
+func _show_message(message: Message) -> void:
+	match message.type:
+		MessageType.INFO:
+			Output.append_text(message.text)
+		MessageType.WARNING:
+			Output.push_color(EditorInterface.get_base_control().get_theme_color("warning_color", &"Editor"))
+			Output.add_image(EditorInterface.get_base_control().get_theme_icon("Warning", "EditorIcons"))
+			Output.append_text(" " + message.text)
+			Output.pop()
+		MessageType.ERROR:
+			Output.push_color(EditorInterface.get_base_control().get_theme_color("error_color", &"Editor"))
+			Output.add_image(EditorInterface.get_base_control().get_theme_icon("Error", "EditorIcons"))
+			Output.append_text(" " + message.text)
+			Output.pop()
+	Output.newline()
+
+
+class Message extends Object:
+	var text: String
+	var type: MessageType
+	
+	func _init(t: String, mt: MessageType) -> void:
+		text = t
+		type = mt
--- addons/starground-sdk/sg_modtools.gd
+++ addons/starground-sdk/sg_modtools.gd
@@ -1,21 +1,21 @@
 @tool
 extends EditorPlugin
 
-var toolPanel
-var sdkLoggerOutput: VBoxContainer
+var toolPanel: Control
+var sdkLoggerPanel: Control
 var log_panel: RichTextLabel
 var sdkAutoLoads = [
 	["SDKData", "res://addons/starground-sdk/global/sdk_data.gd"],
 	["SDKInternal", "res://addons/starground-sdk/scripts/sdk_internal_utils.gd"],
-	["LoggerUtils", "res://addons/starground-sdk/scripts/logger_utils.gd"],
 	["SDKDataUtils", "res://addons/starground-sdk/scripts/sdk_data_utils.gd"],
 	["CreateModUtils", "res://addons/starground-sdk/scripts/create_mod_logic.gd"],
 	["ExportModUtils", "res://addons/starground-sdk/scripts/export_mod_logic.gd"]
 ]
 
+
 func _enter_tree() -> void:
 	# Initialization of the plugin goes here.
-	create_sdk_logger()
+	await create_sdk_logger()
 
 	# toolBar = preload("res://addons/starground-sdk/exporter_tool.tscn").instantiate()
 	# add_control_to_container(EditorPlugin.CONTAINER_TOOLBAR, toolBar)
@@ -24,29 +24,34 @@ func _enter_tree() -> void:
 	toolPanel.name = _get_plugin_name()
 	get_editor_interface().get_editor_main_screen().add_child(toolPanel)
 	_make_visible(false)
-
-
+	
+	# We have to await for a frame to be able to use the SDKLogger in the setup of autoloads
+	await get_tree().process_frame
+	
 	_setup_autoload_sdk()
-
 
 
 func _exit_tree() -> void:
 	# Clean-up of the plugin goes here.
 	_remove_autoload_sdk()
 
-	if sdkLoggerOutput:
-		remove_control_from_bottom_panel(sdkLoggerOutput)
-		sdkLoggerOutput.free()
+	if sdkLoggerPanel:
+		remove_autoload_singleton("SDKLogger")
+		remove_control_from_bottom_panel(sdkLoggerPanel)
+		sdkLoggerPanel.queue_free()
 
 	if toolPanel:
-		toolPanel.free()
+		toolPanel.queue_free()
+
 
 func _has_main_screen() -> bool:
 	return true
 
+
 func _make_visible(visible):
 	if toolPanel:
 		toolPanel.visible = visible
+
 
 func _get_plugin_name():
 	return "Starground SDK"
@@ -60,36 +65,24 @@ func _setup_autoload_sdk() -> void:
 	for aLoad in sdkAutoLoads:
 		if not ProjectSettings.has_setting("autoload/" + aLoad[0]):
 			add_autoload_singleton(aLoad[0], aLoad[1])
-			log_message("Added {0} as Autoload.".format([aLoad[0]]))
+			get_node("/root/SDKLogger").info("Added {0} as Autoload.".format([aLoad[0]]))
 			print("[Starground SDK] Added {0} as Autoload.".format([aLoad[0]]))
 		else:
+			get_node("/root/SDKLogger").info("Project already contains {0} singleton.".format([aLoad[0]]))
 			print("[Starground SDK] Project already contains {0} singleton.".format([aLoad[0]]))
+
 
 func _remove_autoload_sdk():
 	for aLoad in sdkAutoLoads:
 		remove_autoload_singleton(aLoad[0])
 
 
-# TODO: Move this to a better implementation that I can globally reference within mutltiple locations.
 func create_sdk_logger():
-	sdkLoggerOutput = VBoxContainer.new()
-	log_panel = RichTextLabel.new()
-	log_panel.bbcode_enabled = true
-	log_panel.size_flags_vertical = Control.SIZE_EXPAND_FILL
-	log_panel.size_flags_horizontal = Control.SIZE_EXPAND_FILL
-	sdkLoggerOutput.add_child(log_panel)
-
-	add_control_to_bottom_panel(sdkLoggerOutput, "Starground SDK")
-
-# Function to log standard messages with BBCode support
-func log_message(message: String):
-	log_panel.append_text("[color=#f26f0c][Starground SDK][/color] - " + message + "\n")
-	# log_panel.append_bbcode("[color=#f26f0c][Starground SDK][/color] - " + message + "\n")
-
-# Function to log warning messages in yellow
-func log_warning(message: String):
-	log_panel.append_bbcode("[color=yellow]" + message + "[/color]\n")
-
-# Function to log error messages in red
-func log_error(message: String):
-	log_panel.append_bbcode("[color=red]" + message + "[/color]\n")
+	add_autoload_singleton("SDKLogger", "res://addons/starground-sdk/scripts/sdk_logger.gd")
+	# We need to await for the next frame so the SDKLogger exists
+	await get_tree().process_frame
+	sdkLoggerPanel = preload("res://addons/starground-sdk/interfaces/sdk_logger_panel.tscn").instantiate()
+	# Have to use get_node instead of referencing SDKLogger directly because it doesn't exist when the 
+	# script is getting parsed and will throw an error. This only happens with this script.
+	get_node("/root/SDKLogger").Output = sdkLoggerPanel.Output
+	add_control_to_bottom_panel(sdkLoggerPanel, "Starground SDK")
